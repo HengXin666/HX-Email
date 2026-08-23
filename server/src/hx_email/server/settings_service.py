99fd3d9e3363ab1810738574d079fd472c715e9a
Heng_Xin <282000500@qq.com>
2026-08-23 13:57:07 +0800

    chore(release): 版本号提升至 0.10.0

--- server/src/hx_email/server/settings_service.py
+++ server/src/hx_email/server/settings_service.py
@@ -11,7 +11,7 @@ from hx_email.security import ENCRYPTED_PREFIX, decrypt_secret, encrypt_secret
 from hx_email.server.settings.utils import normalize_external_api_keys
 from hx_email.server.settings.validation import validate_callback_url
 
-VERSION: str = os.environ.get("HX_EMAIL_APP_VERSION", "0.9.0")
+VERSION: str = os.environ.get("HX_EMAIL_APP_VERSION", "0.10.0")
 PROJECT_REPOSITORY_URL: str = "https://github.com/HengXin666/HX-Email"
 _SETTING_UPSERT_SQL: str = (
     "INSERT INTO system_settings (key, value) VALUES (?, ?) "
